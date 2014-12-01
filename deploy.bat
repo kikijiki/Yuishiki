9e899c75ba726c4eb70a4a1a3c9b4192b50cd63d
KIKIJIKI <dev@kikijiki.com>
2014-12-01 09:27:20 +0900

    Add mac deployment

--- deploy.bat
+++ deploy.bat
@@ -10,15 +10,20 @@ winrar a -afzip -ibck -r bin\game.love lib
 winrar a -afzip -ibck -r bin\game.love summon
 winrar a -afzip -ibck -r bin\game.love yuishiki
 
-echo Building windows exe...
-copy /b platforms\windows\love.exe+bin\game.love bin\yuishiki.exe > nul
+echo Building windows...
+copy /b platforms\windows\love.exe+bin\game.love bin\yuishiki-win.exe > nul
+
+echo Building mac...
+del bin\yuishiki-mac.zip > nul
+copy bin\game.love platforms\mac\yuishiki.app\Contents\Resources > nul
+winrar a -afzip -ibck -r bin\yuishiki-mac.zip platforms\mac > nul
 
 echo Building android apk...
 copy bin\game.love platforms\android\assets\  > nul
 cd platforms\android
 call ant -q debug > nul
 cd %~dp0
-copy platforms\android\bin\love_android_sdl2-debug.apk bin\yuishiki.apk > nul
+copy platforms\android\bin\love_android_sdl2-debug.apk bin\yuishiki-and.apk > nul
 
 echo Deploying android apk...
 adb install -r bin\yuishiki.apk
